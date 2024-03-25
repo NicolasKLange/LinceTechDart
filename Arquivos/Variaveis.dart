a2bb1ade3538cf1636e409ca31568c0c01c73bcf
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-25 14:24:39 -0300

    Variaveis e Valores Padroes

--- Arquivos/Variaveis.dart
+++ Arquivos/Variaveis.dart
@@ -1,10 +1,16 @@
 void main(){
 
-  //PODE USAR QUALQUER FORMA PARA DECLARAR AS VARIÁVEIS
+  //1. PODE USAR QUALQUER FORMA PARA DECLARAR AS VARIÁVEIS
+
 
   //var    nomeCompleto = 'Nicolas Lange';
   //Object nomeCompleto = 'Nicolas Lange';
   String   nomeCompleto = 'Nicolas Lange';
+
+  //2. SEMPRE TEMQUE INICIALIZAR A VARIAVEL
+  String nome = '';
+  //3. PARA RECEBER VALOR NULL COLOLCA ? LOGO APÓS AO TIPO DE VARIAVEL
+  // EXEMPLO > String? nome = null;
 
   print ("Hello World!!");
   print(nomeCompleto);
