ae0088e38bb1085b901f7d5169addb73bf428325
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-25 14:46:19 -0300

    Usando Late

---
+++ Arquivos/LateVariaveis.dart
@@ -0,0 +1,11 @@
+//O late retarda a inicialização da variavel, algum momento
+//você irá utiliza-la antes de mostrar, não pode esquecer de
+//iniciar a variável, se não dá erro
+
+late double valorTotal;
+
+void main(){
+  valorTotal = 1000.0;
+
+  print(valorTotal);
+}
