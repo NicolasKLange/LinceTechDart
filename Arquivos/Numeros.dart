add8ee0ff1a9ab9c27f5c0febca8029a72c174f2
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-25 20:46:10 -0300

    Trabalhando com números

---
+++ Arquivos/Numeros.dart
@@ -0,0 +1,7 @@
+void main() {
+  int    idade   = 17;      //Valores inteiros, exemplo: idade, qtdeAlunos, etc...
+  double salario = 1141.32; //Valores com decimais, exemplo: salario, altura, etc...
+
+  print("A idade de Nicolas é " + idade.toString()); //.toString() > converte o valor int para um texto
+  print("Salário: $salario"); //Pode usar o $ para concatena a variavel também
+}
