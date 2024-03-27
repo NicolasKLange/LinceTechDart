04fc9232714df8ca0a2e459ce96aec217378c820
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-27 14:26:29 -0300

    Atividade 2 > Finalizada

---
+++ ap1_variaveis/ap2.dart
@@ -0,0 +1,13 @@
+void main() {
+  //*** ENTRADA DE DADOS ***
+  const valor1 = 7.0;
+  const valor2 = 8.0;
+  const valor3 = 5.3;
+  const valor4 = 9.7;
+  const media  = (valor1 + valor2 + valor3 + valor4) / 4;
+
+  // *** SAIDA DE DADOS ***
+  print(
+    'A media entre [ ${valor1.toStringAsFixed(1)} , ${valor2.toStringAsFixed(1)} , ${valor3.toStringAsFixed(1)} , ${valor4.toStringAsFixed(1)} ] = ${media.toStringAsFixed(1)}',
+  );
+}
