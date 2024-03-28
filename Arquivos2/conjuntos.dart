03ffa478a31f58a8ae6c875c847727229cf53b61
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-28 13:58:48 -0300

    Aula de Conjuntos

---
+++ Arquivos2/conjuntos.dart
@@ -0,0 +1,9 @@
+//Os conjuntos constituem uma coleção de elementos únicos e não ordenados
+
+void main () {
+  var nomes1 = {"Nicolas", "Gustavo", "Amanda"};         //Uma forma de istanciar
+  Set<String> nomes2 = {"Nicolas", "Gustavo", "Amanda"}; //Outra forma de istanciar
+
+  print(nomes1);
+  print(nomes2);
+}
