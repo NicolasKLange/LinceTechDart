88444f6c8388848735549f5092454382db7d991c
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-29 09:28:10 -0300

    Arquivos sobre funções

---
+++ Arquivos3/funcoes.dart
@@ -0,0 +1,33 @@
+//Function > consegue acessar de qualquer lugar do programa, é uma classe abstrata
+//funcoes tem um retorno, nome e seus parametros
+
+void main() {
+  /*
+  1° forma de criar uma função
+  void Function() minhaFuncao = (){  //void > nao retorna nada  ; () nao recebe nenhum parametro
+    print("Minha Função!!");
+  };
+
+  2° forma de criar uma função
+  var minhaFuncao = () {
+    print("Minha Função!!");
+  }
+  */
+
+  //3° forma de criar uma função, usar quando o return da funcao é pequeno
+  void minhaFuncao() => print ("Minha Função!!");
+
+  //Função que retorna uma soma de dois numeros
+  num Function(num, num) soma = (num val1, num val2) {
+    return val1 + val2;
+  };
+
+  //Imprimindo a funcao
+  minhaFuncao();
+
+  //Imprimindo a funcao de soma
+  print("A soma é ${soma(10, 10)}");
+
+}
+  //pode usar a funcao dentro, ou fora do escopo, desse jeito a funcao esta encurtada
+  num soma(num val1, num val2) => val1 + val2;
