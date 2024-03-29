c8b9f873583e8b46f1cf73db3670cfc3e28eb236
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-29 10:50:48 -0300

    Funcao como Objetos

--- Arquivos3/funcaoObjetos.dart
+++ Arquivos3/funcaoObjetos.dart
@@ -2,7 +2,7 @@ void main() {
   var list = [1, 2, 3];
 
   list.forEach(calcularElement);   // .forEach > pega cada elemento e vai interar e disponibiliza na varaivel
-}
+}  
 
 void printElement(int element) => print(element);
 var  calcularElement = (int element) => print(element * 2);
