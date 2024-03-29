90e83ad60870b58380672c095ab37428ca5c0610
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-29 19:26:23 -0300

    Arquivos sobre operadores

---
+++ Arquivos4/opAritmeticos.dart
@@ -0,0 +1,13 @@
+void main() {
+  print(5 + 2); //Soma
+  print(5 - 2); //Subtração
+  print(5 * 2); //Multiplicação
+  print(5 / 2); //Divisão
+  print(5 % 2); //O resto, exemplo > (1 1) (1 1) 1 = 1 > Usado para verificar numeros pares e impares
+  print(5 ~/ 2); //Quantas vezes deu para agrupar os valores, exemplo > (1 1) (1 1) 1 = 2 grupos
+
+  //Adicionar 1 ao valor
+  var numero = 1;
+
+  print("Adicionado 1 ao valor 1 = ${++numero}"); //colocar ++ antes para logo que for imprimir o valor ja somar
+}
