fa93274419bbd397b6c1756ea51510bc20945718
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-29 12:08:30 -0300

    Gerar senha aleatório

--- Arquivos3/desafioSenha.dart
+++ Arquivos3/desafioSenha.dart
@@ -1,31 +1,6 @@
 /*
     FAZER O LOGIN DE UM USUARIO COM O EMAIL DEFINIDO E CRIAR UMA SENHA ALEATORIA
 */
-/*
-void main() {
-  // Email do usuário
-  String email = "nicolas@gmail.com";
-
-  // Geração de senha aleatória de 5 números
-  String senha = gerarSenha();
-
-  // Exibição do email e senha gerada
-  print("Email: $email");
-  print("Senha: $senha");
-}
-
-String gerarSenha() {
-  // Lista de numeros possíveis na senha
-  List<int> numeros = List.generate(10, (valores) => valores);
-
-  // Embaralhador de numeros
-  numeros.shuffle();
-
-  // Selecionar os 5 primeiros numeros embaralhados para formar a senha
-  String senha = numeros.sublist(0, 5).join();
-  return senha;
-}
-*/
 
 // Import para gerar números aleatórios
 import 'dart:math';
@@ -45,7 +20,7 @@ void main() {
 }
 
 // Função que mostra os numeros da senha
-void imprimirLogin(List<int> senha, email) {
+void imprimirLogin(List<int> senha, String email) {
   if (senha.length > 0) {
     print('Email: $email \nSenha: ${senha.join("")}');
   }
