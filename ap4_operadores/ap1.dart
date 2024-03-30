aa40a9ecc65ddf44be19c728ab3412f1c978087e
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-30 14:06:30 -0300

    Atividade 1 > Finalizada

---
+++ ap4_operadores/ap1.dart
@@ -0,0 +1,21 @@
+void main() {
+  // Lista de anos
+  final anos = [1988, 1993, 2000, 2016, 2100, 2300];
+
+  // Verificando todos os anos
+  for (var ano in anos) {
+    print(anoBissexto(ano)
+        ? 'O ano $ano é bissexto'
+        : 'O ano $ano nao é bissexto');
+  }
+}
+
+bool anoBissexto(int ano) {
+  if (ano % 100 == 0) {
+    // Anos que terminam em 00 precisam ser divisíveis por 400 para serem bissexto
+    return (ano % 400) == 0;
+  } else {
+    // Anos que nao terminam em 00 precisam ser divisíveis por 4 para serem bissexto
+    return (ano % 4) == 0;
+  }
+}
