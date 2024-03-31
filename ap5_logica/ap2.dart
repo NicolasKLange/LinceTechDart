a618d1da557a29bc3cc932e9e153bcdc89c1b848
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-31 12:08:13 -0300

    Atividade 2 > Finalizada

---
+++ ap5_logica/ap2.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+void main() {
+
+  final random = Random();
+
+  // Laco de repeticao para executar 5 vezes, pq sao 5 numeros
+  for (var indice = 0; indice < 5; indice++) {
+    final numeroAleatorio     = random.nextInt(25) + 1; //26 letras
+    final letraCorrespondente = pegarLetra(numeroAleatorio);
+
+    // Imprimir o num e letra
+    print('Numero $numeroAleatorio -> Letra $letraCorrespondente');
+  }
+}
+
+String pegarLetra(int numero) {
+  int valorLetra = 'A'.codeUnitAt(0); //Numero do alfabeto da letra A
+  int codLetra = valorLetra + numero - 1;
+  return String.fromCharCode(codLetra);
+}
