be5a227660d7b1a98138f31f5a4fd063b4f9b8ab
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-31 08:01:34 -0300

    Comando condicional

---
+++ Arquivos5/comandoCondi.dart
@@ -0,0 +1,11 @@
+void main() {
+  final idade = 17;
+
+  if(idade >= 18) {
+    print("Maior de idade");
+  } else if(idade >=13) {
+    print("Adolescente");
+  } else {
+    print("Menor de idade");
+  }
+}
