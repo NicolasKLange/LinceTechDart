94fae7168c382fab5a8119aabe5e7faeec993ad3
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-03-31 11:56:49 -0300

    Utilizando switchCase

--- Arquivos5/switchCase.dart
+++ Arquivos5/switchCase.dart
@@ -1,22 +1,48 @@
-void main(List<String> args) {
-  final String operacao = args[0];
-  final int n1 = int.parse(args[1]);
-  final int n2 = int.parse(args[2]);
+//import utilizado para fazer a entrada de dados
+import 'dart:io';
 
-  if (operacao == "soma") {
-    soma(n1, n2);
-  } else if (operacao == "sub") {
-    sub(n1, n2);
-  } else if (operacao == "mult") {
-    mult(n1, n2);
-  } else if (operacao == "div") {
-    div(n1, n2);
-  } else {
-    print("Operacao invalida!!");
+void main() {
+
+  //menu para escolher a operacao
+  print("Escolha a operação: ");
+  print("1 - Soma");
+  print("2 - Subtração");
+  print("3 - Divisão");
+  print("4 - Multiplicação");
+
+  //coletar a operacao escolhida
+  int opcao = int.parse(stdin.readLineSync()!);
+
+  //coletar o primeiro número
+  print("Digite o primeiro número:");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  //coletar o segundo número
+  print("Digite o segundo número:");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  //switch para verificar a operacao escolhida
+  switch (opcao) {
+    case 1:
+      soma(num1, num2);
+      break;
+    case 2:
+      subtracao(num1, num2);
+      break;
+    case 3:
+      divisao(num1, num2);
+      break;
+    case 4:
+      multiplicacao(num1, num2);
+      break;
+    default:
+      print("Opção inválida");
+      return;
   }
 }
 
-void soma(int n1, int n2) => print("Resultado: ${n1 + n2}");
-void sub(int n1, int n2) => print("Resultado: ${n1 - n2}");
-void mult(int n1, int n2) => print("Resultado: ${n1 * n2}");
-void div(int n1, int n2) => print("Resultado: ${n1 / n2}");
+//funcoes para fazer o calculo da operacao escolhida
+void soma          (double n1, double n2) => print("Resultado = ${n1 + n2}");
+void subtracao     (double n1, double n2) => print("Resultado = ${n1 - n2}");
+void divisao       (double n1, double n2) => print("Resultado = ${n1 / n2}");
+void multiplicacao (double n1, double n2) => print("Resultado = ${n1 * n2}");
