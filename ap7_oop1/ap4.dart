32efc3fba1f81af630371d870e4f0d6c748d09a7
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-04-02 15:09:21 -0300

    Atividade 4 > Finalizada

---
+++ ap7_oop1/ap4.dart
@@ -0,0 +1,46 @@
+import 'dart:math';
+
+void main() {
+  final random = Random();
+
+  final pessoa = Pessoa();
+
+  // Colocando os dados para cada um
+  pessoa.nome = "Nicolas";
+  pessoa.idade = random.nextInt(99) + 1;
+  pessoa.altura = (random.nextDouble() * 1.3) + 1;
+
+  // Imprimindo os dados da pessoa
+  print("Nome: ${pessoa.nome}");
+  print("Idade: ${pessoa.idade}");
+  print("Altura: ${pessoa.altura.toStringAsFixed(2)}");
+}
+
+// Criando a classe
+class Pessoa {
+  String _nome = '';
+  int _idade = 0;
+  double _altura = 0.0;
+
+  int get idade => _idade;
+
+  set idade(int valor) {
+    if (valor >= 0) {
+      _idade = valor;
+    } else {
+      print("A idade deve ser um valor maior ou igual a zero.");
+    }
+  }
+
+  String get nome => _nome;
+
+  set nome(String valor) {
+    _nome = valor;
+  }
+
+  double get altura => _altura;
+
+  set altura(double valor) {
+    _altura = valor;
+  }
+}
